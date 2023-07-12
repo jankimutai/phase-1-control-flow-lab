55fb98255e4d04db9874834466a3e4f0bee64fae
Kimutai <jankimsingoei@gmail.com>
2023-07-12 14:21:38 +0300

    Done with the task

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feetCount){
   // Write your code here!
+  if (feetCount <400){
+    return ('This one is on me!');
+  }else if (feetCount > 2000 && feetCount<= 2500 ){
+    return 'I will gladly take your thirty bucks.';
+  } else( feetCount>2500);{
+    return 'No can do.'
+  }
+}
+function ternaryCheckCity(cityCheck){
+  // Write your code here!
+  if(cityCheck ==='NYC'){
+    return "Ok, sounds good.";
+  }else {
+    return "No go.";
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tipCheck){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if(tipCheck === 'generous'){
+    return "Thank you so much.";
+  } else if(tipCheck === 'not as generous'){
+    return "Thank you.";
+  }else if(tipCheck === 'thanks for everything'){
+    return "Bye.";
+  }
 }
